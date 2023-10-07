8590264ca5c68199c16000e1bc98f7e5dd426166
Andres Vazquez <andres.vazquez.martin@gmail.com>
2023-10-07 09:28:48 +0200

    lifting state up example

---
+++ src/components/Task.jsx
@@ -0,0 +1,28 @@
+function Task(props) {
+  const { task, toggleTaskDone } = props;
+  return (
+    <div className="task-card">
+      <div className="task-card-half">
+        <h1>{task.name}</h1>
+        {task.isDone ? ( // <== UPDATE
+          <span>DONE </span>
+        ) : (
+          <span>PENDING ⌛</span>
+        )}
+
+        <h2> Task Description </h2>
+        <p>{task.description}</p>
+
+        <button className="add" onClick={() => toggleTaskDone(task._id)}>
+          {task.isDone ? ( // <== UPDATE
+            <span>UNDO </span>
+          ) : (
+            <span>✔️</span>
+          )}
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Task;
